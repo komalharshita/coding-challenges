8e1ad786cdb4026e620f0ff04ac3ae437c388d81
komal harsh27 <komal.sony234@gmail.com>
2025-10-02 18:46:32 +0530

    added solutions

--- ~Courses/Udemy - Java Programming Bootcamp/Module 4 Advanced Java Programming Concepts/ExceptionDemo.java
+++
@@ -1,9 +0,0 @@
-public class ExceptionDemo {
-    public static void main(String[] args) {
-        try {
-            int result = 10 / 0;
-        } catch (ArithmeticException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-}
