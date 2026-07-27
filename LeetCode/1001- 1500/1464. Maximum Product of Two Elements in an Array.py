1a74152260fb8ba313f4f1ba6dbd1a28f7e86eff
Komal Harshita <komal.sony234@gmail.com>
2026-07-27 11:13:59 +0530

    27 july

---
+++ LeetCode/1001- 1500/1464. Maximum Product of Two Elements in an Array.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        max1 = 0
+        max2 = 0
+
+        for num in nums:
+            if num >= max1:
+                max2 = max1
+                max1 = num
+            elif num > max2:
+                max2 = num
+            continue
+        return (max1 - 1) * (max2 - 1)
+
+
+# optimized solution: 
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        max1 = 0  
+        max2 = 0  
+
+        for num in nums:
+            if num >= max1:
+                max2 = max1  
+                max1 = num
+            elif num > max2:
+                max2 = num
+        
+        return (max1 - 1) * (max2 - 1)
